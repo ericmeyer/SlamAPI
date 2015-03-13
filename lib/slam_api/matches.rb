c5409b75ef3513a3bc460b7af8bf72392a090f65
esanmiguelc <esanmiguel@gmail.com>
2015-03-13 17:40:49 -0500

    Adding filter to remove completed games   - Added route and corresponding method to generate json for pending     games array   - adding filter to filter out games that have already been completed

--- lib/slam_api/matches.rb
+++ lib/slam_api/matches.rb
@@ -5,6 +5,10 @@ module SlamAPI
         matches
       end
 
+      def pending_matches
+        matches.select { |match| match.completed? == false }
+      end
+
       def matches
         @matches ||= []
       end
@@ -16,20 +20,33 @@ module SlamAPI
       def destroy_all
         @matches = []
       end
+
+      def find_by_id(id)
+        matches.select {|match| match.id == id}.first
+      end  
     end
 
-    attr_reader :id, :player_one, :player_two
+    attr_reader :id, :player_one, :player_two, :completed
 
     def initialize(attributes)
       @id = SecureRandom.uuid 
       @player_one = attributes[:player_one]
       @player_two = attributes[:player_two]
+      @completed = false
     end
 
     def create
       if (valid?)
         Matches.add(self)
       end
+    end
+
+    def completed?
+      completed
+    end
+
+    def complete_game
+      @completed = true
     end
 
     def valid?
