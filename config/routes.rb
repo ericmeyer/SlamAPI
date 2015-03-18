60ca74b9c38d605bf429fb9931705be5725d0928
esanmiguelc <esanmiguel@gmail.com>
2015-03-17 22:27:07 -0500

    removing unused pending game route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get 'matches', to: 'matches#index'
-  get 'matches', to: 'matches#pending_matches'
   post 'matches', to: 'matches#create'
 end
