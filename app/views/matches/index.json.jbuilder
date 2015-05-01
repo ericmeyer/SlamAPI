56332c667665240d14f14d36aab54c3ad38d481f
esanmiguelc <esanmiguel@gmail.com>
2015-05-01 16:38:56 -0500

    Feature: Sorting Matches  - removing completed attribute reader  - adding created_at to matches  - polished tests

--- app/views/matches/index.json.jbuilder
+++ app/views/matches/index.json.jbuilder
@@ -3,4 +3,5 @@ json.array! @matches do |match|
   json.player_one match.player_one
   json.player_two match.player_two
   json.id match.id
+  json.created_at match.created_at
 end
--- spec/slam_api/matches_spec.rb
+++ spec/slam_api/matches_spec.rb
@@ -2,28 +2,42 @@ require "slam_api/matches"
 
 RSpec.describe SlamAPI::Matches do
 
-  let(:valid_attributes) {{ :player_one => "taka", :player_two => "eric" }}
+  let(:valid_attributes) {{ :player_one => "taka", :player_two => "eric"}}
 
   context ".all" do
-    it "retrieves all the matches" do
-      match = SlamAPI::Matches.new(valid_attributes)
-      match.create
+    it "retrieves all with single match" do
+      match = create_match(valid_attributes)
 
       expect(SlamAPI::Matches.all).to include(match)
+      expect(SlamAPI::Matches.pending_matches).to eq(1)
+    end
+
+
+    it "retrieves all with two matches" do
+      match = create_match(valid_attributes)
+      match2 = create_match(valid_attributes)
+
+      expect(SlamAPI::Matches.all).to include(match)
+      expect(SlamAPI::Matches.pending_matches).to eq(2)
+    end
+
+    it "retrieves matches sorted by creation date" do
+      match = create_match(valid_attributes)
+      match2 = create_match(valid_attributes)
+
+      expect(SlamAPI::Matches.all.first).to eq(match)
     end
   end
 
   it "assigns a uuid to a match" do
-    match = SlamAPI::Matches.new(valid_attributes)
-    match.create
+    match = create_match(valid_attributes)
 
     expect(SlamAPI::Matches.all[0].id).to_not be_nil
   end
 
   context ".find_by_id" do
     it "finds the match by its id" do
-      match = SlamAPI::Matches.new(valid_attributes)
-      match.create
+      match = create_match(valid_attributes)
 
       expect(SlamAPI::Matches.find_by_id(match.id)).to eq(match)
     end
@@ -31,8 +45,8 @@ RSpec.describe SlamAPI::Matches do
 
   context "#destroy_match" do
     it "deletes a match" do
-      match = SlamAPI::Matches.new(valid_attributes)
-      match.create
+      match = create_match(valid_attributes)
+
       SlamAPI::Matches.destroy_match(match.id)
       
       expect(SlamAPI::Matches.all).to be_empty
@@ -44,12 +58,12 @@ RSpec.describe SlamAPI::Matches do
       attributes = {
         :player_one => "taka"
       }
-      match = SlamAPI::Matches.new(attributes)
+      match = create_match(attributes)
       expect(match.valid?).to eq(false)
     end
 
     it "is valid when two players are present" do
-      match = SlamAPI::Matches.new(valid_attributes)
+      match = create_match(valid_attributes)
       expect(match.valid?).to eq(true)
     end
 
@@ -57,9 +71,14 @@ RSpec.describe SlamAPI::Matches do
       attributes = {
         :player_one => "taka"
       }
-      match = SlamAPI::Matches.new(attributes)
-      match.create
+      match = create_match(attributes)
       expect(SlamAPI::Matches.all.empty?).to eq(true) 
     end  
+  end
+
+  def create_match(valid_attributes)
+    match = SlamAPI::Matches.new(valid_attributes)
+    match.create
+    match
   end
 end
